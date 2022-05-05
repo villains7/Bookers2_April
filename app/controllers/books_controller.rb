4a18696d9d64319773502422995768254f60cecd
villainsff7 <villainsff790@gmail.com>
2022-05-05 05:21:41 +0000

    [practice] html to Slim

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,19 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def create
+    @book = Book.new(book_params)
+
   end
 
   def edit
