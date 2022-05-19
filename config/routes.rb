bb870b977ecddeec31a97cb0cfcb9faadc85bdd7
villainsff7 <villainsff790@gmail.com>
2022-05-19 10:51:17 +0000

    [Add] users_view

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
 
   resources:books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
